0fb45b229e4963e2cd0047f983e831ec83bf9c2d
dearis02 <kdarista02@gmail.com>
2025-02-18 15:17:11 +0800

    feat(service-category): add new public endpoint to get all service categories

--- internal/routes/service_category.go
+++ internal/routes/service_category.go
@@ -21,4 +21,6 @@ func NewServiceCategory(g *gin.Engine, serviceCategoryHandler handler.ServiceCat
 
 func (r *ServiceCategory) Register(mw middleware.Auth) {
 	r.g.GET("/common/v1/service-categories", mw.Authenticated, r.serviceCategoryHandler.GetAll)
+
+	r.g.GET("/v1/service-categories", r.serviceCategoryHandler.GetAll)
 }
